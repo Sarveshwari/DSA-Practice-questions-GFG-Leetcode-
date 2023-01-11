1e25944a6be62e07322effdd141ed8db52ed2614
Sarveshwari <87422258+Sarveshwari@users.noreply.github.com>
2023-01-11 15:22:00 +0300

    Q24

---
+++ longconsecseq.cpp
@@ -0,0 +1,40 @@
+class Solution{
+  public:
+    // arr[] : the input array
+    // N : size of the array arr[]
+    
+    //Function to return length of longest subsequence of consecutive integers.
+    int findLongestConseqSubseq(int arr[], int N)
+    {
+        unordered_set<int>s;
+        
+        for(int i=0; i<N; i++)
+        {
+            s.insert(arr[i]);
+        }
+        
+        int ans=0;
+        
+        for(int i=0; i<N; i++)
+        {
+            if(s.find(arr[i]-1)!=s.end())
+            continue;
+            
+            else
+            {
+                int count=0;
+                int current=arr[i];
+                
+                while(s.find(current)!=s.end())
+                {
+                    count++;
+                    current++;
+                }
+                
+                ans=max(ans, count);
+            }
+        }
+        
+        return ans;
+    }
+};
