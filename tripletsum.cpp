e0796906fc8ba77d375f28e837024b397d2fc0b3
Sarveshwari <87422258+Sarveshwari@users.noreply.github.com>
2023-01-13 15:26:42 +0300

    Q28

---
+++ tripletsum.cpp
@@ -0,0 +1,31 @@
+class Solution{
+    public:
+    //Function to find if there exists a triplet in the 
+    //array A[] which sums up to X.
+    bool find3Numbers(int A[], int n, int X)
+    {
+       sort(A, A+n);
+       
+       for(int i=0; i<n; i++)
+       {
+           int low=i+1; 
+           int high=n-1;
+           int y=X-A[i];
+           while(low<high)
+           {
+                if(A[low]+A[high]==y)
+           return 1;
+           
+           else if(A[low]+A[high]>y)
+           high--;
+           
+           else
+           low++;
+               
+           }
+       }
+       
+       return 0;
+    }
+
+};
