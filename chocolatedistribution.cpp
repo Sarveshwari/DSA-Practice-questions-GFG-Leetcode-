b6925abbe49fde652e708c00c270f4251151a2e9
Sarveshwari <87422258+Sarveshwari@users.noreply.github.com>
2023-01-15 19:22:36 +0300

    Q30

---
+++ chocolatedistribution.cpp
@@ -0,0 +1,19 @@
+class Solution
+{
+public:
+    long long findMinDiff(vector<long long> a, long long n, long long m)
+    {
+
+        sort(a.begin(), a.end());
+
+        long long mindiff = INT_MAX, mini = 0;
+
+        for (int i = 0; (i + m - 1) < n; i++)
+        {
+            mini = a[i + m - 1] - a[i];
+            mindiff = min(mindiff, mini);
+        }
+
+        return mindiff;
+    }
+};
