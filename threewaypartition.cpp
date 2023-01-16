ef62ed62b794078c61bfd83d8e80d5f9bd2ddcb2
Sarveshwari <87422258+Sarveshwari@users.noreply.github.com>
2023-01-16 12:49:22 +0300

    Q32

---
+++ threewaypartition.cpp
@@ -0,0 +1,34 @@
+class Solution
+{
+public:
+    // Function to partition the array around the range such
+    // that array is divided into three parts.
+
+    // Dutch Flag Algorithm Approach ( used whenver an array is to be partitioned.)
+
+    void threeWayPartition(vector<int> &array, int a, int b)
+    {
+        int start = 0;
+        int end = array.size() - 1;
+        int i = 0;
+
+        while (i <= end)
+        {
+            if (array[i] < a)
+            {
+                swap(array[i], array[start]);
+                i++;
+                start++;
+            }
+            else if (array[i] > b)
+            {
+                swap(array[i], array[end]);
+                end--;
+            }
+            else
+            {
+                i++;
+            }
+        }
+    }
+};
