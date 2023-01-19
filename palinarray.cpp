ed9248bab08a965d73f252ce840f5d41b1ba6805
Sarveshwari <87422258+Sarveshwari@users.noreply.github.com>
2023-01-19 23:11:42 +0300

    Q34

---
+++ palinarray.cpp
@@ -0,0 +1,24 @@
+class Solution
+{
+public:
+    int PalinArray(int a[], int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            int temp = a[i];
+            int rev = 0;
+            int r;
+
+            while (temp > 0)
+            {
+                r = temp % 10;
+                temp = temp / 10;
+                rev = rev * 10 + r;
+            }
+
+            if (rev != a[i])
+                return 0;
+        }
+        return 1;
+    }
+};
