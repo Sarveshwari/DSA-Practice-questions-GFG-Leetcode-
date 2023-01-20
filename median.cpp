82de81e317699f13a324669fa5e5ca8c8a4c275d
Sarveshwari <87422258+Sarveshwari@users.noreply.github.com>
2023-01-20 23:03:34 +0300

    school median

---
+++ median.cpp
@@ -0,0 +1,24 @@
+class Solution
+{
+public:
+	public:
+		int find_median(vector<int> v)
+		{
+		   int n=v.size();
+		   int median;
+		   sort(v.begin(), v.end());
+		   
+		   if(n%2==0)
+		   {
+		       n=n-1;
+		       median = (v[n/2]+v[(n/2)+1])/2;
+		   }
+		   else
+		   {
+		       n=n-1;
+		       median = v[(n+1)/2];
+		   }
+		   
+		   return median;
+		}
+};
