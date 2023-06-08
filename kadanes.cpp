a1f96211c70d2f3a9e666a4329ada57fdcb994cd
Sarveshwari <87422258+Sarveshwari@users.noreply.github.com>
2023-06-08 23:57:42 +0530

    Create kadanes.cpp

---
+++ kadanes.cpp
@@ -0,0 +1,18 @@
+ long long maxSubarraySum(int arr[], int n){
+        
+        long long sum=0;
+        long long maxi=arr[0];
+        
+        for(int i=0; i<n; i++)
+        {
+            sum=sum+arr[i];
+            
+            maxi=max(sum,maxi);
+            
+            if(sum<0)
+            sum=0;
+        }
+        
+        return maxi;
+        
+    }
