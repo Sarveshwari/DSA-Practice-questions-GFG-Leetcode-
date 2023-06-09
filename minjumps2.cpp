0808aabb7797acc63b37848544a9aaf09c0d6122
Sarveshwari <87422258+Sarveshwari@users.noreply.github.com>
2023-06-10 00:14:28 +0530

    Update minjumps2.cpp

--- minjumps2.cpp
+++ minjumps2.cpp
@@ -1,1 +1,31 @@
-
+int minJumps(int arr[], int n){
+        // Your code here
+        if(n<=1)
+        return 0;
+        
+        if(arr[0]==0)
+        return -1;
+        
+        int steps=arr[0];
+        int maxreach=arr[0];
+        int jump=1;
+        
+        for(int i=1; i<n; i++)
+        {
+            if(i==n-1)
+            return jump;
+            
+            maxreach=max(maxreach, i+arr[i]);
+            steps--;
+            
+            if(steps==0){
+            jump++;
+            
+            if(i>=maxreach)
+            return -1;
+            
+            steps = maxreach-i;
+            } 
+        }
+        return -1;
+    }
