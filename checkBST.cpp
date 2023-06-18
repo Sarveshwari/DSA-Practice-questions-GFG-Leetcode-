74a03b54ea3849256ed5e06ccc0741e952017c5f
Sarveshwari <87422258+Sarveshwari@users.noreply.github.com>
2023-06-18 19:29:02 +0530

    Create checkBST.cpp

---
+++ checkBST.cpp
@@ -0,0 +1,22 @@
+int func(Node*root, Node* &prev){
+        if(root){
+            if(!(func(root->left, prev)))
+            return false;
+            
+            if(prev!=NULL && root->data <= prev->data)
+            return false;
+            
+            prev=root;
+            
+            return func(root->right, prev);
+        }
+        
+        
+        
+    }
+    bool isBST(Node* root) 
+    {
+        Node* prev=NULL;
+      
+        return func(root, prev);
+    }
