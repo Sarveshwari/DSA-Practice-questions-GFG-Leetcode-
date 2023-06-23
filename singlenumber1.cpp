cb357bea018e8eebe2f919384852d445136d7873
Sarveshwari <87422258+Sarveshwari@users.noreply.github.com>
2023-06-23 13:52:50 +0530

    Create singlenumber1.cpp

---
+++ singlenumber1.cpp
@@ -0,0 +1,21 @@
+ vector<int> productExceptSelf(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> result(n,1);
+
+        for(int i=1; i<n; i++)
+        {
+            result[i]=result[i-1]*nums[i-1];
+        }
+
+        int right_prod=nums[n-1];
+
+        for(int i=n-2; i>=0; i--)
+        {
+            result[i]*=right_prod;
+            right_prod*=nums[i];
+        }
+
+
+
+    return result;
+    }
