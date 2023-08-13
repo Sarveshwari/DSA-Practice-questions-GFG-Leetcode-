12de95f49dbe45a1cb0f1189a72784ddb7f00b4b
Sarveshwari <87422258+Sarveshwari@users.noreply.github.com>
2023-08-13 22:48:53 +0530

    Create detectLoopReturnNode.cpp

---
+++ detectLoopReturnNode.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+
+        
+    if(head==NULL)
+    {
+        return 0;
+    }
+
+    ListNode* slow=head;
+    ListNode* fast=head;
+    ListNode* entry=head;
+
+    while(fast->next && fast->next->next)
+    {
+        slow=slow->next;
+        fast=fast->next->next;
+
+        if(slow==fast)
+        {
+            while(slow!=entry)
+            {
+                slow=slow->next;
+                entry=entry->next;
+            }
+            return slow;
+        }
+       
+    }
+
+     return NULL;
+        
+    }
+
+};
