b476de8ae9162f542a4a2641d58c583770b42267
Sarveshwari <87422258+Sarveshwari@users.noreply.github.com>
2023-08-14 15:38:55 +0530

    Create removeDuplicatesinSortedLL.cpp

---
+++ removeDuplicatesinSortedLL.cpp
@@ -0,0 +1,30 @@
+Node *removeDuplicates(Node *head)
+{
+ // your code goes here
+ 
+ if(head==NULL)
+ {
+     return NULL;
+ }
+ 
+ Node* curr=head;
+ 
+ while(curr != NULL)
+ {
+     if((curr->next!=NULL) && curr->data == curr->next->data)
+     {
+         Node* next_next= curr->next->next;
+         Node* nodetodelete=curr->next;
+         delete(nodetodelete);
+         curr->next=next_next;
+         
+         
+     }
+     else
+     {
+         curr=curr->next;
+     }
+ }
+ 
+ return head;
+}
