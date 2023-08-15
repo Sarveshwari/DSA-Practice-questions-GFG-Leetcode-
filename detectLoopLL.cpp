3f9719810208b18858e81a3a1441b4b146dd7e93
Sarveshwari <87422258+Sarveshwari@users.noreply.github.com>
2023-08-15 21:42:07 +0530

    Update detectLoopLL.cpp

--- detectLoopLL.cpp
+++ detectLoopLL.cpp
@@ -6,8 +6,7 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
-class Solution {
-public:
+
     bool hasCycle(ListNode *head) {
          if(head==NULL)
     {
@@ -32,4 +31,4 @@ public:
     return false;
       
     }
-};
+
