878358cbc02d76d388badf855abb00377cec7e3c
Sarveshwari <87422258+Sarveshwari@users.noreply.github.com>
2023-12-25 19:02:12 +0300

    Create deletemiddlestack.cpp

---
+++ deletemiddlestack.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h> 
+
+void solve(stack<int>&s, int count , int size)
+{
+   if(count==size/2)
+   {
+      s.pop();
+      return;
+   }
+
+   int num=s.top();
+   s.pop();
+
+   solve(s, count+1, size);
+
+   s.push(num);
+
+
+}
+void deleteMiddle(stack<int>&inputStack, int N){
+	
+   // Write your code here
+   int count=0; 
+   solve(inputStack, count, N);
+   
+}
