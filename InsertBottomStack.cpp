d5aba96e308ca441fcdf18d19290fd65e422925d
Sarveshwari <87422258+Sarveshwari@users.noreply.github.com>
2023-12-27 11:48:21 +0300

    Create InsertBottomStack.cpp

---
+++ InsertBottomStack.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h> 
+
+void solve(stack<int>& s, int x)
+{
+    if(s.empty())
+    {
+        s.push(x);
+        return;
+    }
+
+    int num=s.top();
+    s.pop();
+
+    solve(s,x);
+    s.push(num);
+
+}
+
+stack<int> pushAtBottom(stack<int>& myStack, int x) 
+{
+    // Write your code here.
+    solve(myStack, x);
+    return myStack;
+}
+v
