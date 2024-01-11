6ff8a5d625dca9d716f65d3fdd95a0b0ee43cc45
Sarveshwari <87422258+Sarveshwari@users.noreply.github.com>
2024-01-11 22:28:59 +0300

    Create QueueUsingArray.cpp

---
+++ QueueUsingArray.cpp
@@ -0,0 +1,47 @@
+/* 
+
+The structure of the class is
+class MyQueue {
+private:
+    int arr[100005];
+    int front;
+    int rear;
+
+public :
+    MyQueue(){front=0;rear=0;}
+    void push(int);
+    int pop();
+};
+ */
+
+//Function to push an element x in a queue.
+void MyQueue :: push(int x)
+{
+        // Your Code
+        if(rear==100005)
+        {
+            return;
+        }
+        else
+        {
+            arr[rear]=x;
+            rear++;
+        }
+}
+
+//Function to pop an element from queue and return that element.
+int MyQueue :: pop()
+{
+        // Your Code 
+        if(front==rear)
+        {
+            return -1;
+        }
+        else{
+            int value=arr[front];
+            arr[front]=-1;
+            front++;
+            return value;
+        }
+        
+}
